e508b3a9769d628df3fdb7d6900df4cc8ba48faf
branRoberts <brandonkrob12@gmail.com>
2026-04-16 12:16:59 -0400

    Fourth Commit Did the for loop. I have look at this again to see how it's doing it

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+
+            if (i == 1)
+                System.out.println("LAUNCH");
+        }
+    }
+}
