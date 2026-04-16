8f7d8f69fa413f5dd307ab8fc6e4eff2eb0bac8c
branRoberts <brandonkrob12@gmail.com>
2026-04-16 12:05:07 -0400

    Second Commit Added a commit

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+// a while loop that displays the print five times
+        int i = 0;
+        while(i < 5){
+            System.out.println("I love Java");
+            i++;
+        }
+    }
 }
